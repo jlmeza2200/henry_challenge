ae9e6e50d5bb32dba4ca9670071967ebf9446960
Checkpoints one <104238791+soyhenry-checkpoints-1@users.noreply.github.com>
2022-06-18 04:12:38 -0300

    Initial commit

---
+++ 02.js
@@ -0,0 +1,21 @@
+/* 
+  Importante: 
+  No modificar ni el nombre ni los argumetos que reciben las funciones, sólo deben escribir
+  código dentro de las funciones ya definidas. 
+  No comentar la funcion 
+*/
+function masFrecuente(array) {
+  // La funcion llamada 'masFrecuente' recibe como argumento un array de numeros enteros
+  // y debe devolver el número entero que mas veces aparece (el más frecuente).
+  // ej:
+  // masFrecuente([1,1,2,3,4]) => 1
+  // masFrecuente([3,2,3,2,3,3]) => 3
+  // Nota: Los numeros pueden NO estar ordenados
+
+  // Tu código aca:
+
+}
+
+// No modifiques nada debajo de esta linea //
+
+module.exports = masFrecuente
